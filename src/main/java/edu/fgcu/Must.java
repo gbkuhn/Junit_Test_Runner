53eced5eb6862c523fe1275f4dd6f46e6dfde4c9
gbkuhn <kuhn.geoff@gmail.com>
2015-04-16 08:59:25 -0400

    Annotation improvements

--- src/main/java/edu/fgcu/Must.java
+++ src/main/java/edu/fgcu/Must.java
@@ -5,6 +5,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import main.java.edu.fgcu.Desired.None;
+import main.java.edu.fgcu.Runner_info.Priority;
 
 /**
  * Created by geoffreykuhn on 4/5/15.
@@ -15,6 +16,12 @@ import main.java.edu.fgcu.Desired.None;
 public @interface Must {
     String name() default "";
     Class<? extends Throwable> expected() default None.class;
+    
+    public enum Priority {
+        LOW, MEDIUM, HIGH
+    }
+
+    Priority priority() default Priority.HIGH;
 
     static class None extends Throwable {
     }
